f5cb5b5602342b27e996eac96e139f7d3485002e
Patrick Holody <patryk.holody@gmail.com>
2019-04-12 01:06:14 +0200

    MainActivity update

--- app/src/main/java/com/example/sudokuapp/MainActivity.java
+++ app/src/main/java/com/example/sudokuapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sudokuapp;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -12,14 +13,20 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
@@ -40,10 +47,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private final String uriString = "content://media/internal/images/media";
     private Bitmap currentBitmap;
     private Bitmap originalBitmap;
+
+    //TODO OCR
+    ImageView image;
+    TextView text;
+
+    private static final String TAG = "MyActivity";
+
+    private Bitmap resizedBitmap;
     private Mat originalMat;
-    private Mat cropped;
+    private Mat cropped, resizedStart;
+    // TODO make all comments on the methods
+
+
+    // TODO Clean code
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -64,27 +86,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO OCR
+        image = (ImageView) findViewById(R.id.ocr_image_view);
+        text = (TextView) findViewById(R.id.ocr_text_view);
+        //TODO OCR
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
 
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://media/internal/images/media"));
-                startActivityForResult(intent, 0);
-            }
-        });
+        fab.setOnClickListener(this::onClickLoadImage);
 
         FloatingActionButton fab1 = findViewById(R.id.fab1);
-        fab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SudokuGridDetection();
+
+        fab1.setOnClickListener(this::onClickSudokuGridDetection);
+
+
+    }
+
+    //TODO OCR
+    public void getTextFromImage(View view) {
+        Bitmap bitmap = currentBitmap;
+        Context context = getApplicationContext();
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+        if (!textRecognizer.isOperational()|| originalBitmap == null) {
+            Toast.makeText(context, "You need to load an image first! OR Cloud not get the Number", Toast.LENGTH_SHORT).show();
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+
+            StringBuilder numberToShow = new StringBuilder();
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock myItem = items.valueAt(i);
+                numberToShow.append(myItem.getValue());
+                numberToShow.append("");
+
             }
-        });
+
+            // Display In the window
+            Toast.makeText(context, numberToShow.toString(), Toast.LENGTH_LONG).show();
+
+            // Display in place of text
+            text.setText(numberToShow.toString());
+        }
 
     }
 
@@ -105,10 +154,8 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        } else if (id == R.id.open_gallery) {
-            Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://media/internal/images/media"));
-            startActivityForResult(intent, 0);
-        } else if (id == R.id.SGD) {
+        } else if (id == R.id.open_gallery) onClickLoadImage(null);
+        else if (id == R.id.SGD) {
             SudokuGridDetection();
         }
 
@@ -118,13 +165,16 @@ public class MainActivity extends AppCompatActivity {
     private void loadImageToImageView() {
         ImageView imgView = (ImageView) findViewById(R.id.image_view);
         imgView.setImageBitmap(currentBitmap);
+
     }
+
+
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        Log.d("LensActivity", requestCode + " " + data + " " + resultCode + " " + RESULT_OK);
         if (requestCode == 0 && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
@@ -144,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
             options.inSampleSize = 2;
 
             Bitmap temp = BitmapFactory.decodeFile(picturePath, options);
-
+            // TODO używać jak najmnniejszy TRY  w  Catch opisać dlaczego
             //Get orientation information
             int orientation = 0;
             try {
@@ -162,10 +212,12 @@ public class MainActivity extends AppCompatActivity {
 
             //Convert Bitmap to Mat
             Bitmap tempBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
-            originalMat = new Mat(tempBitmap.getHeight(), tempBitmap.getWidth(), CvType.CV_8U);
+            originalMat = new Mat(tempBitmap.getHeight() / 2, tempBitmap.getWidth() / 2, CvType.CV_32F);
             Utils.bitmapToMat(tempBitmap, originalMat);
 
-            currentBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, false);
+            //TODO cut Log
+            Log.v(TAG, " currentBitmap  ");
+            currentBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
             loadImageToImageView();
         }
     }
@@ -219,7 +271,7 @@ public class MainActivity extends AppCompatActivity {
         Mat grayMat = new Mat();
         Mat blur1 = new Mat();
 
-
+        // TODO Clean Code
         //Converting the image to grayscale
         Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
         Imgproc.GaussianBlur(grayMat, blur1, new Size(5, 5), 0);
@@ -247,26 +299,66 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        Mat displayMat = grayMat;
+        Mat displayMat = originalMat;
         Point[] points = biggest.toArray();
-        cropped = new Mat();
-        int t = 3;
+
+        // TODO delete Log
+        Log.v(TAG, "width"+String.valueOf(displayMat.width()));
+        Log.v(TAG, "height"+String.valueOf(displayMat.height()));
         if (points.length >= 4) {
             // draw the outer box
-            Imgproc.line(displayMat, new Point(points[0].x, points[0].y), new Point(points[1].x, points[1].y), new Scalar(255, 0, 0), 2);
-            Imgproc.line(displayMat, new Point(points[1].x, points[1].y), new Point(points[2].x, points[2].y), new Scalar(255, 0, 0), 2);
-            Imgproc.line(displayMat, new Point(points[2].x, points[2].y), new Point(points[3].x, points[3].y), new Scalar(255, 0, 0), 2);
-            Imgproc.line(displayMat, new Point(points[3].x, points[3].y), new Point(points[0].x, points[0].y), new Scalar(255, 0, 0), 2);
+
+            Imgproc.circle(displayMat, new Point(points[0].x, points[0].y), 2,new Scalar(0, 255, 0),10);
+            Imgproc.circle(displayMat, new Point(points[1].x, points[1].y), 2,new Scalar(0, 255, 0),10);
+            Imgproc.circle(displayMat, new Point(points[2].x, points[2].y), 2,new Scalar(0, 255, 0),10);
+            Imgproc.circle(displayMat, new Point(points[3].x, points[3].y), 2,new Scalar(0, 255, 0),10);
+
             // crop the image
-            Rect R = new Rect(new Point(points[0].x - t, points[0].y - t), new Point(points[2].x + t, points[2].y + t));
+            Rect R = new Rect(new Point(points[0].x  , points[0].y  ), new Point(points[2].x  , points[2].y ));
             if (displayMat.width() > 1 && displayMat.height() > 1) {
                 cropped = new Mat(displayMat, R);
             }
         }
-        Utils.matToBitmap(displayMat, currentBitmap);
-        loadImageToImageView();
+        // TODO delete Log
+        Log.v(TAG, "cropped width "+String.valueOf(cropped.width()));
+        Log.v(TAG, "cropped height "+String.valueOf(cropped.height()));
 
+
+        displayImage(cropped);
 
     }
+    private void displayImage(Mat image)
+    {
+        // convert to bitmap:
+        Bitmap bitMap = Bitmap.createBitmap(image.cols(), image.rows(),Bitmap.Config.RGB_565);
+        Utils.matToBitmap(image, bitMap);
 
+        // find the imageview and draw it!
+        ImageView iv = (ImageView) findViewById(R.id.image_view);
+        iv.setImageBitmap(bitMap);
+        currentBitmap = bitMap;
+    }
+
+
+
+    private void onClickSudokuGridDetection(View view) {
+        if (originalBitmap == null) {
+            Context context = getApplicationContext();
+            CharSequence text = "You need to load an image first!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        } else {
+            SudokuGridDetection();
+        }
+    }
+
+    private void onClickLoadImage(View viev) {
+        {
+            Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse(uriString));
+            startActivityForResult(intent, 0);
+        }
+    }
 }
--- app/src/main/java/com/example/sudokuapp/OcrActivity.java
+++ app/src/main/java/com/example/sudokuapp/OcrActivity.java
@@ -1,4 +1,15 @@
 package com.example.sudokuapp;
 
-public class OcrActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OcrActivity extends Activity {
+//    ImageView image;
+//    TextViev text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
+
